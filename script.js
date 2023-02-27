12cdad74208442729fb2a4d43468f6c33f74fcb5
Husseinturfah <@husseinturfah>
2023-02-28 01:07:51 +0200

    Declared some variables along with a dictionary.

---
+++ script.js
@@ -0,0 +1,20 @@
+const input = document.getElementById('input')
+const man = document.getElementById('man')
+const new_game = document.getElementById('new-game')
+const answer = document.getElementById('answer')
+
+let options = {
+  fruits : [
+    'Apple',
+    'Banana',
+    'Mango',
+  ],
+  animals : [
+    "Lion",
+    'Tiger',
+    'Elephant',
+  ],
+
+}
+console.log(options.fruits[0])
+console.log(options.animals[1])
