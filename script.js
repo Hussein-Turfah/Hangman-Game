dec6592f9c7d954255f2dcc220a5c9cbec08f5fe
Husseinturfah <@husseinturfah>
2023-02-28 09:36:47 +0200

    Added Hangman Styles.

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 const input = document.getElementById('input')
 const man = document.getElementById('man')
-const new_game = document.getElementById('new-game')
+const new_game_button = document.getElementById('new-game')
 const answer = document.getElementById('answer')
 
 let options = {
-  fruits : [
-    'Apple',
-    'Banana',
-    'Mango',
-  ],
-  animals : [
-    "Lion",
-    'Tiger',
-    'Elephant',
-  ],
-
-}
+  words : ['Apple','Banana','Mango',"Lion",'Tiger','Elephant',]}
 console.log(options.fruits[0])
 console.log(options.animals[1])
+
+let word = options.words[Math.floor(Math.random() * options.words.length)].toLowerCase();
+let count = 0
+let incorrect_guesses = 0
+let chosen_word = ''
+
