63783994ee97dd74b25f40d4d175d105390188c5
Husseinturfah <@husseinturfah>
2023-02-28 15:59:37 +0200

    Added a function for validating the right letters and showing the hangman for wrong letters.

--- script.js
+++ script.js
@@ -15,13 +15,79 @@ const splitWord = () => {
 }
 splitWord(options.words)
 
-function replaceLetter(){ //to replace the chosen word with underscores(as lines)
-  let display_string = '';
-  let current_index = 0;
-  for (let i = 0; i < word.length; i++) {
-    display_string += '_,';
-  }
-  console.log(display_string)
-}
-replaceLetter(word)
+//new
 
+function check(word) {
+  let displayString = '';
+  let currentIndex = 0;
+  let life = 6
+
+  //start first with underscores for each letter in the word chosen by system
+  for (let i = 0; i < word.length; i++) {
+    displayString += '_';
+  }
+  output.textContent = displayString;
+
+  document.addEventListener('keydown', function(event) {
+    // extract the pressed key from the event object
+    const key = event.key;
+      if(life>0){
+        if (word.includes(key)) {
+          for (let i = 0; i < word.length; i++) {
+            if (word[i] === key) {
+            // if the key is correct, update the display string
+              displayString = displayString.slice(0, i) + key + displayString.slice(i + 1);}
+          }
+        }
+        else{
+          switch(life){
+            case 1:
+              this.getElementById('left-leg').style.display = 'block';
+              document.removeEventListener('keydown', arguments.callee);
+              output.innerHTML = "Game Over!"
+              break;
+            case 2:
+              this.getElementById('right-leg').style.display = 'block';
+              break;
+            case 3:
+              this.getElementById('left-arm').style.display = 'block';
+              break;
+            case 4:
+              this.getElementById('right-arm').style.display = 'block';
+              break;
+            case 5:
+              this.getElementById('body').style.display = 'block';
+              break;
+            case 6:
+                this.getElementById('head').style.display = 'block';
+                break;
+          }
+          life -= 1
+          console.log(life)
+        }
+      }else{
+        document.removeEventListener('keydown', arguments.callee);
+        alert('Game Over!')
+        return
+      }
+    
+
+    // update the output element on the screen
+    output.textContent = displayString;
+    //check for missing letters
+    if (!displayString.includes('_')) {
+      alert('You Won!');
+      // remove the event listener to prevent further input
+      document.removeEventListener('keydown', arguments.callee);
+    }
+  });
+}
+
+
+check(word)
+
+
+switch(hangman){
+  case 1:
+
+}
