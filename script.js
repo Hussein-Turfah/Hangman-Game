92667b9c45539ad00af0593a85cd085bf6be6300
Husseinturfah <@husseinturfah>
2023-02-28 11:40:36 +0200

    Added a function for replacing letters with underscores.

--- script.js
+++ script.js
@@ -2,20 +2,26 @@ const input = document.getElementById('input')
 const man = document.getElementById('man')
 const new_game_button = document.getElementById('new-game')
 const answer = document.getElementById('answer')
+const output = document.getElementById('test1');
 
 let options = {
   words : ['Apple','Banana','Mango',"Lion",'Tiger','Elephant',]}
-console.log(options.words)
-
 const word = options.words[Math.floor(Math.random() * options.words.length)].toLowerCase();
 
-console.log(word)
-document.getElementById('test').innerHTML = word
+document.getElementById('test').innerHTML = word.split()
 const splitWord = () => {
-  let letter = word.split('')
+  let letter = word.split()
   console.log(letter)
 }
-arrWord(options.words)
+splitWord(options.words)
 
+function replaceLetter(){ //to replace the chosen word with underscores(as lines)
+  let display_string = '';
+  let current_index = 0;
+  for (let i = 0; i < word.length; i++) {
+    display_string += '_,';
+  }
+  console.log(display_string)
+}
+replaceLetter(word)
 
-// let incorrect_guesses = 0
